0316a1aa19e14519c6209f6ef6a8d62c83184978
Joshh4993 <joshedwards2k19@gmail.com>
2022-08-25 15:29:00 +0100

    Update [mid].js

--- pages/meet/[mid].js
+++ pages/meet/[mid].js
@@ -5,28 +5,21 @@ import styles from '../../styles/Meetup.module.css'
 import MapContainer from '../../components/MapContainer'
 import { useRouter } from 'next/router'
 
-export default async function Home(props) {
-  const router = useRouter()
-  const { mid } = router.query
-  const res = await fetch(
-    `https://api.where2meet.uk/meets/${mid}`
-  );
-  const meet = await res.json()
-
+function MeetPage(props) {
   return (
     <Layout>
       <div className={styles.meetups}>
-        {meet && (
+        {props.meet && (
           <>
-            <div key={meet._id} className={styles.card}>
-              <h3 className={styles.gridTitle}>{meet.name}</h3>
-              <h4 className={styles.gridLocLat}>Lat: {meet.lat}</h4>
-              <h4 className={styles.gridLocLong}>Long: {meet.long}</h4>
-              <p className={styles.detailsLs}>{meet.description}</p>
+            <div key={props.meet._id} className={styles.card}>
+              <h3 className={styles.gridTitle}>{props.meet.name}</h3>
+              <h4 className={styles.gridLocLat}>Lat: {props.meet.lat}</h4>
+              <h4 className={styles.gridLocLong}>Long: {props.meet.long}</h4>
+              <p className={styles.detailsLs}>{props.meet.description}</p>
             </div>
             <div className={styles.card}>
               <div className={styles.gridMap}>
-                <MapContainer lat={meet.lat} long={meet.long} name={meet.name} />
+                <MapContainer lat={props.meet.lat} long={props.meet.long} name={props.meet.name} />
               </div>
               <a className={styles.gridDirectionsButton} href={`#`}>Get Directions</a>
             </div>
@@ -36,3 +29,15 @@ export default async function Home(props) {
     </Layout>
   )
 }
+
+export async function getServerSideProps() {
+  const router = useRouter()
+  const { mid } = router.query
+  const res = await fetch(
+    `https://api.where2meet.uk/meets/${mid}`
+  );
+  const meet = await res.json()
+  return { props: { meet } }
+}
+
+export default MeetPage
